0a610f25748735f1ce10a8d1c436b9e799f99f98
Luis Antonio Mendoza <luismendoza.dev@gmail.com>
2019-09-12 13:40:51 -0500

    Functions file created with essential constants

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+
+// Define theme folder route
+define( 'THEMEROOT', get_stylesheet_directory_uri() );
+
+// Define images folder route
+define( 'IMAGES', THEMEROOT . '/img' );
+
+// Disable file edition through admin panel
+define( 'DISALLOW_FILE_EDIT', true );
