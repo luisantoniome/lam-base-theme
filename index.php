83212470192e4d4584213140d6e736db74711e07
Luis Antonio Mendoza <luismendoza.dev@gmail.com>
2019-09-27 19:51:10 -0500

    Basic loop code

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php get_header(); ?>
 
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+  
+  <h2><?php the_title() ;?></h2>
+  <?php the_excerpt(); ?>
+
+<?php endwhile; else : ?>
+
+	<p><?php esc_html_e( 'Sorry, no content to show.' ); ?></p>
+  
+<?php endif; ?>
+
 <?php get_footer(); ?>
