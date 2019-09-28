ab8f5e697c3c8accf71f2a1ef11926eb85986f57
Luis Antonio Mendoza <luismendoza.dev@gmail.com>
2019-09-28 10:42:00 -0500

    singular.php template

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
   
-  <h2><?php the_title() ;?></h2>
+  <h2><a href="<?php the_permalink(); ?>"><?php the_title() ;?></a></h2>
   <?php the_excerpt(); ?>
 
 <?php endwhile; else : ?>
